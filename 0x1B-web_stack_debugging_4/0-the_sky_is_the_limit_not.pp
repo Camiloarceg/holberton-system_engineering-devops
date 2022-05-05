78905455c8b4494f37f612bca2bf0298038e7af8
Camiloarceg <camiloarceg@gmail.com>
2022-05-05 15:13:50 -0500

    fix task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 exec{ 'fix-nginx':
-      command  => 'sed -i "s/-n 15/-n 4096/g" /etc/default/nginx',
-      provider => 'shell'
+  path    => '/bin',
+  command => "sed -i 's/15/4096/g' /etc/default/nginx"
 }
 exec{'restart-nginx':
-  command  => 'sudo service nginx restart',
-  provider => 'shell'
+  path    => '/etc/init.d',
+  command => 'nginx restart'
 }
