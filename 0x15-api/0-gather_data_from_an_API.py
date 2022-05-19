3fdca53888aabc1d8184b5c8e483402c4e75b545
Camiloarceg <camiloarceg@gmail.com>
2022-05-19 14:13:40 -0500

    addsasa

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     TOTAL_NUMBER_OF_TASKS = len(requests.get(
         f"{url}/todos?userId={argv[1]}").json())
     print(f"Employee {EMPLOYEE_NAME} is done with\
-        tasks({len(NUMBER_OF_DONE_TASKS)}/{TOTAL_NUMBER_OF_TASKS})")
+        tasks({len(NUMBER_OF_DONE_TASKS)}/{TOTAL_NUMBER_OF_TASKS}):")
     for task in NUMBER_OF_DONE_TASKS:
         TASK_TITLE = task["title"]
         print(f"\t {TASK_TITLE}")
