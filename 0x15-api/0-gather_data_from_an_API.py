270fb7c9ee72faec8e91217cb406876510f974fb
Camiloarceg <camiloarceg@gmail.com>
2022-05-19 14:26:26 -0500

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,13 +9,16 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    EMPLOYEE_NAME = requests.get(f"{url}/users/{argv[1]}").json()["name"]
+
+    TOTAL_NUMBER_OF_TASKS = len(
+        requests.get("{}/todos?userId={}".format(url, argv[1])).json())
     NUMBER_OF_DONE_TASKS = requests.get(
-        f"{url}/todos?userId={argv[1]}&completed=true").json()
-    TOTAL_NUMBER_OF_TASKS = len(requests.get(
-        f"{url}/todos?userId={argv[1]}").json())
-    print(f"Employee {EMPLOYEE_NAME} is done with tasks(\
-{len(NUMBER_OF_DONE_TASKS)}/{TOTAL_NUMBER_OF_TASKS}):")
+        "{}/todos?userId={}&completed=true".format(url, argv[1])).json()
+    EMPLOYEE_NAME = requests.get(
+        "{}/users/{}".format(url, argv[1])).json()["name"]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), TOTAL_NUMBER_OF_TASKS))
+
     for task in NUMBER_OF_DONE_TASKS:
-        TASK_TITLE = task["title"]
-        print(f"\t {TASK_TITLE}")
+        print("\t {}".format(task["title"]))
