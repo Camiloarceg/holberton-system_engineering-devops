dd23c836c035ab5aa034bcc3a468c1705b308480
Camiloarceg <camiloarceg@gmail.com>
2022-05-19 13:43:08 -0500

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    url = "https://jsonplaceholder.typicode.com/"
-    EMPLOYEE_NAME = requests.get(f"{url}users/{argv[1]}").json()["name"]
+    url = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_NAME = requests.get(f"{url}/users/{argv[1]}").json()["name"]
     NUMBER_OF_DONE_TASKS = requests.get(
-        f"{url}todos?userId={argv[1]}&completed=true").json()
+        f"{url}/todos?userId={argv[1]}&completed=true").json()
     TOTAL_NUMBER_OF_TASKS = len(requests.get(
-        f"{url}todos?userId={argv[1]}").json())
+        f"{url}/todos?userId={argv[1]}").json())
     print(f"Employee {EMPLOYEE_NAME} is done with tasks\
         ({len(NUMBER_OF_DONE_TASKS)}/{TOTAL_NUMBER_OF_TASKS})")
     for task in NUMBER_OF_DONE_TASKS:
