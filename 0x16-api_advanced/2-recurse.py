7c4d48f3fc8bd4792d057edd550be6e3074e419d
Camiloarceg <camiloarceg@gmail.com>
2022-05-21 11:37:22 -0500

    adasfsadfg

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,13 +2,14 @@
 """ Return list containing the titles of all hot articles """
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=""):
     """ Query Reddit API to return all got articles """
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
     headers = {"User-Agent": "Python3"}
-    response = request.get(
-        url, headers=headers, allow_redirects=False).json().get('data')
     try:
+        response = request.get(url, headers=headers,
+                               allow_redirects=False).json().get('data')
         after = response.get('after', None)
         for item in response.get('children'):
             hot_list.append(item.get('data').get('title'))
